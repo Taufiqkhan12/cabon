6e058804124fab81972e7af87afefe444d9266da
Taufiqkhan12 <ktaufiq913@gmail.com>
2025-08-10 16:14:08 +0530

    Update utility function

--- Backend/src/utils/utilityFunction.js
+++ Backend/src/utils/utilityFunction.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import nodemailer from "nodemailer";
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "./ApiError";
 import dotenv from "dotenv";
 dotenv.config();
 
