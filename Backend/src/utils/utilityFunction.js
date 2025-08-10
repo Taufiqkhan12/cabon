437a5dcbd0226215c03b85158b273bfcde59a299
Taufiqkhan12 <ktaufiq913@gmail.com>
2025-08-10 16:20:33 +0530

    Update import

--- Backend/src/utils/utilityFunction.js
+++ Backend/src/utils/utilityFunction.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import nodemailer from "nodemailer";
-import { ApiError } from "./ApiError";
+import { ApiError } from "./ApiError.js";
 import dotenv from "dotenv";
 dotenv.config();
 
